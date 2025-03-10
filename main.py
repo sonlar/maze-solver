6db46f2f81ad182dd0a914ad9278ad6d59aa1bd3
sonlar <z5bewil7@anonaddy.me>
2025-03-10 20:17:57 +0100

    Create class skeleton

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import Tk, Canvas
+
+class Window:
+    def __init__(self, width height):
+        pass
+
+    def redraw(self):
+        pass
+
+    def wait_for_close(self):
+        pass
+    def close(self):
+        pass
