8e3d909ce8a32d369be352bc3fbdda6a0c8f257d
sonlar <z5bewil7@anonaddy.me>
2025-03-10 20:39:16 +0100

    Implement base structure so window opens

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from tkinter import Tk, Canvas
 
+
 class Window:
-    def __init__(self, width height):
-        pass
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze-solver")
+        canvas = Canvas(self.__root, width=width, height=height)
+        canvas.pack()
+        self.__in_progress = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close())
 
     def redraw(self):
-        pass
+        self.__root.update()
+        self.__root.update_idletasks()
 
     def wait_for_close(self):
-        pass
+        self.__in_progress = True
+        while self.__in_progress:
+            self.redraw()
+
     def close(self):
-        pass
+        self.__in_progress = False
+
+
+if __name__ == "__main__":
+    window = Window(500, 500)
+    window.wait_for_close()
